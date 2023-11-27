0e10f301b0e99fa2ebd3a511c64189b5cbc4b157
swetakarar <kararsweta@gmail.com>
2023-11-27 15:07:45 +0530

    add:search bar and hero section

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import styles from "../../style";
+
+import hero from "../../assets/images/hero.avif";
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
@@ -25,7 +28,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard h-screen">
       {/* Data Upload Form */}
-      <div className="data-upload-form">
+      {/* <div className="data-upload-form">
         <form onSubmit={handleUpload}>
           <input
             type="text"
@@ -35,30 +38,62 @@ const Dashboard = () => {
           />
           <button type="submit">Upload Data</button>
         </form>
-      </div>
+      </div> */}
 
       {/* Search Bar */}
-      <div className="search-bar">
+      {/* <div className="search-bar">
         <input
           type="text"
           value={searchTerm}
           onChange={handleSearch}
           placeholder="Search data"
         />
-      </div>
+      </div> */}
 
       {/* Data Cards */}
-      <div className="data-cards">
+      {/* <div className="data-cards">
         {filteredData.length > 0 ? (
           filteredData.map((item) => (
             <div key={item.id} className="data-card">
               <p>{item.detail}</p>
-              {/* Optional: Add Edit/Delete buttons here */}
             </div>
           ))
         ) : (
           <p>No Results Found</p>
         )}
+      </div> */}
+      <div className="heroSection ">
+        <div
+          className={`bg-[#073a55] flex justify-between ${styles.marginX}  ${styles.paddingX} ${styles.paddingY} h-[80vh] rounded-3xl `}
+        >
+          <div className="texts">
+            <h1 className={`${styles.heading} text-white`}>
+              Explore Your Dream Home
+            </h1>
+            <p className={`${styles.subheading} text-white mt-10`}>
+              Embark on captivating tours to discover the <br /> perfect
+              residence for you and your family
+            </p>
+            <div className="searchbar mt-20 shadow-xl">
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearch}
+                placeholder="Search Property"
+                className="px-4 py-6 w-[800px] rounded-xl absolute z-10"
+              />
+            </div>
+          </div>
+          <div className="image  relative">
+            <div className="bg-white  rounded-2xl shadow-2xl">
+              <img
+                src={hero}
+                alt="image"
+                className=" w-[30rem] h-[40rem] mx-auto p-2 rounded-2xl "
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/style.js
+++ src/style.js
@@ -1,9 +1,9 @@
 const styles = {
   heading:
-    " md:text-[54px] text-[30px] xs:text-[28px] text-white md:leading-[76.8px] leading-[58.8px] ",
+    " md:text-[60px] text-[30px] xs:text-[28px]  md:leading-[76.8px] leading-[58.8px] font-semibold",
 
   subheading:
-    " md:text-[30px] text-[20px] text-white md:leading-[64.8px] leading-[38.8px] w-full",
+    " md:text-[22px] text-[18px] text-white md:leading-[30.8px] leading-[38.8px] w-full",
 
   paragraph:
     "font-poppins font-extralight text-red md:text-[16px] text-[14px] md:leading-[30.8px] leading-[26.8px] ",
@@ -12,12 +12,12 @@ const styles = {
   flexStart: "flex justify-center items-start",
   flexEnd: "flex  items-end",
 
-  paddingX: "sm:px-16 px-6",
+  paddingX: "sm:px-20 px-6",
   paddingY: "sm:py-16 py-6",
   paddingL: "sm:pl-16 pl-6",
   padding: "sm:px-16 px-6 sm:py-12 py-4",
 
-  marginX: "sm:mx-16 mx-6",
+  marginX: "sm:mx-20 mx-6",
   marginL: "sm:ml-16 ml-6",
   marginY: "sm:my-16 my-6",
 };
